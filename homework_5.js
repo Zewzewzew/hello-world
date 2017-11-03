5603a11912c66b69f4da28393be675752c300be3
Zewzewzew <31189943+Zewzewzew@users.noreply.github.com>
2017-11-04 02:16:09 +0300

    Create homework_5.js

---
+++ homework_5.js
@@ -0,0 +1,29 @@
+//Написать функцию, преобразующую число в объект.
+ //Передавая на вход число от 0 до 999, мы должны получить на выходе объект, 
+ //в котором в соответствующих свойствах описаны единицы, десятки и сотни.
+var number = prompt("Enter number.");
+String(number);
+var object = {};
+if (number.length  == 3){
+var firstNumber = number.charAt(0);
+var secondNumber = number.charAt(1);
+var thirdNumber = number.charAt(2);
+object.firstNumber = firstNumber;
+object.secondNumber = secondNumber;
+object.thirdNumber = secondNumber;
+
+}
+else if(number.length == 2){
+	var firstNumber = number.charAt(0);
+	var secondNumber = number.charAt(1);
+	object.firstNumber = firstNumber;
+	object.secondNumber = secondNumber;
+
+}
+else if(number.length == 1){
+	var firstNumber = number.charAt(0);
+		object.firstNumber = firstNumber;
+
+}
+
+console.log(object);
